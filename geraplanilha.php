2c19f37d9d05b91d026d305f62289b88576c7493
andrefg5 <167633809+andrefg5@users.noreply.github.com>
2025-08-24 20:16:01 -0300

    Update geraplanilha.php
    o XAMPP não tem o php 8.4, mesmo atualizando não funciona, vai json mesmo

--- geraplanilha.php
+++ geraplanilha.php
@@ -1,13 +1,13 @@
 <?php
 include 'conecxao.php';
 
-require 'vendor/autoload.php';//os arquivos ficam aqui
+$arquivoJson = 'livros.json'; 
 
-use PhpOffice\PhpSpreadsheet\Spreadsheet;
-use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+header('Content-Type: application/json');
+header('Content-Disposition: attachment; filename="' . $arquivoJson . '"');
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo = new PDO("mysql:host=$host;dbname=$banco;charset=utf8", $user, $senha);
 } catch (PDOException $e) {
     exit("Erro ao conectar ao banco de dados: " . $e->getMessage());
 }
@@ -15,46 +15,10 @@ try {
 $sql = "SELECT ID, TITULO, EDITORA, AUTOR, IDIOMA, GENERO, IMAGEM, FORMATO, VALOR_COMPRA, ANO_PUBLICACAO, ESTADO FROM LIVRARIA";
 $stmt = $pdo->query($sql);
 
-//cria nova planilha
-$spreadsheet = new Spreadsheet();
-$sheet = $spreadsheet->getActiveSheet();
-
-// Definindo o cabeçalho da planilha
-$sheet->setCellValue('A1', 'ID');
-$sheet->setCellValue('B1', 'TITULO');
-$sheet->setCellValue('C1', 'EDITORA');
-$sheet->setCellValue('D1', 'AUTOR');
-$sheet->setCellValue('E1', 'IDIOMA');
-$sheet->setCellValue('F1', 'GENERO');
-$sheet->setCellValue('G1', 'IMAGEM');
-$sheet->setCellValue('H1', 'FORMATO');
-$sheet->setCellValue('I1', 'VALOR_COMPRA');
-$sheet->setCellValue('J1', 'ANO_PUBLICACAO');
-$sheet->setCellValue('K1', 'ESTADO');
-
-$rowCount = 2;
+$dados = [];
 while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-    $sheet->setCellValue('A' . $rowCount, $row['ID']);
-    $sheet->setCellValue('B' . $rowCount, $row['TITULO']);
-    $sheet->setCellValue('C' . $rowCount, $row['EDITORA']);
-    $sheet->setCellValue('D' . $rowCount, $row['AUTOR']);
-    $sheet->setCellValue('E' . $rowCount, $row['IDIOMA']);
-    $sheet->setCellValue('F' . $rowCount, $row['GENERO']);
-    $sheet->setCellValue('G' . $rowCount, $row['IMAGEM']);
-    $sheet->setCellValue('H' . $rowCount, $row['FORMATO']);
-    $sheet->setCellValue('I' . $rowCount, $row['VALOR_COMPRA']);
-    $sheet->setCellValue('J' . $rowCount, $row['ANO_PUBLICACAO']);
-    $sheet->setCellValue('K' . $rowCount, $row['ESTADO']);
-    $rowCount++;
+    $dados[] = $row;  
 }
 
-// Definindo cabeçalhos para o download
-header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-header('Content-Disposition: attachment;filename="livros.xlsx"');
-header('Cache-Control: max-age=0');
-
-// Escrevendo o arquivo diretamente para o PHP output
-$writer = new Xlsx($spreadsheet);
-$writer->save('php://output');
-exit;
+echo json_encode($dados, JSON_PRETTY_PRINT);
 ?>
